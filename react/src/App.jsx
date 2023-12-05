ab6e9169ef3f1a59f1a91860d7414d3249a3e076
H-kleiber <limnuev25@gmail.com>
2023-12-05 18:35:43 -0500

    api

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/clientes');
+        const data = await response.json();
+        setClientes(data);
+      } catch (error) {
+        console.error('Error al obtener clientes:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div>
-        hola
+        <h1>Clientes:</h1>
+        <ul>
+          {clientes.map(cliente => (
+            <li key={cliente.id}>{cliente.nombre}</li>
+          ))}
+        </ul>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
